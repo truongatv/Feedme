19fef68da8678b791c462cba2656e586cc1a164a
KienLeTrung <kienletrung.1995@gmail.com>
2017-04-26 12:13:52 +0700

    k

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,6 +11,7 @@ class ImagesController < ApplicationController
   # GET /images/1
   # GET /images/1.json
   def show
+    @user = @image.user
   end
 
   # GET /images/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   resources :images
   root 'static_pages#home'
+  
 
-  devise_for :users
+  devise_for :users do
+  	get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/:id', :to => 'users#show', :as => :user
+  end
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
