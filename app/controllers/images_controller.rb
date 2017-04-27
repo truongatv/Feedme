fd5462f23b343c14c9a8295b30072c9ac684077d
quangcsp <nguyenvanquangcsp@gmail.com>
2017-04-27 10:11:25 +0700

    Image_Comment

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,6 +11,7 @@ class ImagesController < ApplicationController
   # GET /images/1
   # GET /images/1.json
   def show
+    @image_comment = ImageComment.new
     @user = @image.user
   end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@ class Image < ApplicationRecord
 	mount_uploader :file, ImageUploader
 
 	belongs_to :user
+	has_many :image_comment, -> { order "created_at DESC"}
 end
