538fdf1a036accd2312609223eedef8a1cc78c44
KienLeTrung <kienletrung.1995@gmail.com>
2017-05-08 23:37:34 +0700

    follow

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+
+gem 'will_paginate'
 # gem 'redis', '~> 3.0'
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
     get '/users/:id', :to => 'users#show', :as => :user
   end
 
-  resources :users
+  resources :users do
+  	member do
+  		get :following, :followers
+  	end
+  end
+
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
