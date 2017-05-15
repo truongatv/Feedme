71c4c55316874094e4b9f3112317387bd2852c61
KienLeTrung <kienletrung.1995@gmail.com>
2017-05-15 23:10:44 +0700

    search ajax and fix show following

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   	end
   end
 
+  get 'live_search' => 'users#live_search'
+
   resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
